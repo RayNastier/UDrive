9341e70184669d3f72bc89d423e54f10d10b5779
RMehdid <rmehdid@harbineldjazair.com>
2022-12-06 19:15:29 +0100

    initialized project

---
+++ UDrive/UDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDriveApp.swift
+//  UDrive
+//
+//  Created by Samy Mehdid on 6/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
